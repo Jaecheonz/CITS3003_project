13d94f2d2f60ec8c2e3efd92f4ba48c7ec3c00ee
Jaecheonz <joshuachinxj289@gmail.com>
2025-05-10 09:42:55 +0800

    added missing edits to vert.glsl in animated entity for task e

--- res/shaders/animated_entity/vert.glsl
+++ res/shaders/animated_entity/vert.glsl
@@ -23,6 +23,9 @@ uniform vec3 specular_tint;
 uniform vec3 ambient_tint;
 uniform float shininess;
 
+// added for task e
+uniform vec2 texture_scale;
+
 // Light Data
 #if NUM_PL > 0
 layout (std140) uniform PointLightArray {
@@ -55,7 +58,9 @@ void main() {
 
     vec3 ws_position = (animation_matrix * vec4(vertex_position, 1.0f)).xyz;
     vec3 ws_normal = normalize(normal_matrix * normal);
-    vertex_out.texture_coordinate = texture_coordinate;
+    
+    // modified for task e
+    vertex_out.texture_coordinate = texture_coordinate * texture_scale;
 
     gl_Position = projection_view_matrix * vec4(ws_position, 1.0f);
 
